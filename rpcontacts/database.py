0694c332585419ca714eba65609e0b96b17a69e5
stranger <alirezaz313313@gmail.com>
2024-04-17 18:57:46 +0330

    .

---
+++ rpcontacts/database.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+def _createconnectiontable():
+    createtablequery = QSqlQuery()
+    return createtablequery.exec(
+        """
+        CREATE TABLE IF NOT EXISTS  contact (
+            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+            name VARCHAR(40) NOT NULL,
+            job VARCHAR(50),
+            email VARCHAR(40) NOT NULL
+        )
+        """
+    )
+    
+
+
+
+
+
+def createconnection(databasename):
+    connection = QSqlDatabase.addDatabase("QSQLITE")
+    connection.setDatabaseName(databasename)
+    
+    if not connection.open():
+        QMessageBox.warning(
+            None,
+            "RP Contact",
+            f"Database error: {connection.lastError().text}",
+        )
+        return False
+    _createconnectiontable()
+    return True
---
+++ rpcontacts/views.py
@@ -0,0 +1,38 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+    QAbstractItemView,
+    QHBoxLayout,
+    QMainWindow,
+    QPushButton,
+    QTableView,
+    QVBoxLayout,
+    QWidget,
+)
+
+class window(QMainWindow):
+    def __init__(self,parent = None ) :
+        super().__init__(parent)
+        self.setWindowTitle("RP Contacts")
+        self.resize(550,250)
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+        self.layout = QHBoxLayout()
+        self.centralWidget.setLayout(self.layout)
+        self.setupUI()
+        
+    def setupUI(self):
+        self.table = QTableView
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+        
+        self.addButton = QPushButton("Add")
+        self.deleteButton = QPushButton("Delete")
+        self.clearbutton = QPushButton("Clear All")
+        
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        layout.addWidget(self.clearbutton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
