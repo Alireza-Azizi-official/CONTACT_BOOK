137d7911277c7308ee65b5293b7c4b7d2019cdae
stranger <alirezaz313313@gmail.com>
2024-04-21 19:37:07 +0330

    .

--- rpcontacts/main.py
+++ rpcontacts/main.py
@@ -20,19 +20,19 @@ def main():
     VALUES (?, ?, ?)
     """
     )
-    data =[
-        ("hassan", "english teacher", "hassan@example.com"),
-        ("reza", "math teacher", "reza@example.com"),
-        ("masoud", "physic teacher", "masoud@example.com"),
-        ("alireza", "art teacher", "alireza@example.com"),
-        ("karim", "Sport teacher", "karim@example.com"),
-    ]
+    # data =[
+    #     ("hassan", "english teacher", "hassan@example.com"),
+    #     ("reza", "math teacher", "reza@example.com"),
+    #     ("masoud", "physic teacher", "masoud@example.com"),
+    #     ("alireza", "art teacher", "alireza@example.com"),
+    #     ("karim", "Sport teacher", "karim@example.com"),
+    # ]
     
-    for name, job, email in data :
-        insertDataQuery.addBindValue(name)
-        insertDataQuery.addBindValue(job)
-        insertDataQuery.addBindValue(email)
-        insertDataQuery.exec()
+    # for name, job, email in data :
+    #     insertDataQuery.addBindValue(name)
+    #     insertDataQuery.addBindValue(job)
+    #     insertDataQuery.addBindValue(email)
+    #     insertDataQuery.exec()
 
     win = Window(None)
     win.show()
